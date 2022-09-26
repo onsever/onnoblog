1a10affa80b9366ca65b73686e7d73a088896fd9
Onurcan Sever <onsever@icloud.com>
2022-09-26 13:04:04 -0400

    changed layout of the sidebar

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -19,20 +19,15 @@ const SideBar = () => {
     return (
         <aside className={styles.sidebar}>
             <section className={styles["sidebar-section"]}>
-                <span className={styles["sidebar-title"]}>About Me</span>
-                <img
-                    src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=800"
-                    alt="Profile Image"
-                    className={styles["sidebar-image"]}
-                />
+                <span className={styles["sidebar-title"]}>Popular Posts</span>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam blanditiis corporis eos eum, exercitationem fugiat, ipsum, iure modi nam numquam optio.</p>
             </section>
             <section className={styles["sidebar-section"]}>
                 <span className={styles["sidebar-title"]}>Categories</span>
                 <ul className={styles["sidebar-list"]}>
                     {categories.map((category) => {
-                        return <Link to={`/?cat=${category.name}`} className={styles.link}>
-                            <li key={category.name} className={styles["sidebar-item"]}>{category.name}</li>
+                        return <Link key={category.name} to={`/?cat=${category.name}`} className={styles.link}>
+                            <li className={styles["sidebar-item"]}>{category.name}</li>
                         </Link>
                     })}
                 </ul>
