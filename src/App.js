93ec2a8e7772ee10c3392228dccc2879000c21e2
Onurcan Sever <onsever@icloud.com>
2022-09-26 06:27:44 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import NavigationBar from "./components/navigationbar/NavigationBar";
+import Home from "./pages/home/Home";
+import PostDetails from "./pages/postdetails/PostDetails";
+import NewPost from "./pages/newpost/NewPost";
+import EditProfile from "./pages/editprofile/EditProfile";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isUserLoggedIn = false;
+
+    return (
+        <BrowserRouter>
+            <NavigationBar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/signup">
+                    {isUserLoggedIn ? <Home /> : <SignUp />}
+                </Route>
+                <Route path="/login">
+                    {isUserLoggedIn ? <Home /> : <Login />}
+                </Route>
+                <Route path="/newpost">
+                    {isUserLoggedIn ? <NewPost /> : <SignUp />}
+                </Route>
+                <Route path="/settings">
+                    {isUserLoggedIn ? <EditProfile /> : <SignUp />}
+                </Route>
+                <Route path="/post/:postId">
+                    <PostDetails />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
