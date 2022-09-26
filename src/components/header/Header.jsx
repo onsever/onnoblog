36092a4ce3afe014db7b5dbe2a817a08ab962649
Onurcan Sever <onsever@icloud.com>
2022-09-26 06:56:09 -0400

    added temporary new post button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { RiAddCircleFill } from "react-icons/ri";
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles["header-image"]}>
-
+                <Link to="/newpost"><RiAddCircleFill className={styles.icon} /></Link>
             </div>
         </header>
     );
